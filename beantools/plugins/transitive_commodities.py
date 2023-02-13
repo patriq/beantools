b066e6ea12825ba101df9432a6fb1da7b937b17d
Andre <patriqdesigns@gmail.com>
2023-02-13 20:19:52 +0100

    Fixed description

--- beantools/plugins/transitive_commodities.py
+++ beantools/plugins/transitive_commodities.py
@@ -1,5 +1,4 @@
-"""A plugin that inserts an additional price directives to help converstion to the operating currencies.
-"""
+"""A plugin that inserts additional price directives to help projecting all commodities to the ledger's operating currencies."""
 from beancount.core import amount, data, prices
 
 __plugins__ = ["generate_transitive_commodities"]
